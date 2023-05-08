3b586d0155b2a3b01ce25dbba8cf1608c0edb4bb
Paul P <85466221+pauljhp@users.noreply.github.com>
2023-05-08 23:11:31 +0800

    added autoencoder

--- src/macroEncoding/deepLearning/dataloader.py
+++ src/macroEncoding/deepLearning/dataloader.py
@@ -1,6 +1,6 @@
 from torch.utils.data import DataLoader, Dataset
 import numpy as np
-from typing import Sequence, Union, Optional, Generator
+from typing import Sequence, Union, Optional, Generator, Tuple
 import pandas as pd
 import torch
 from utils import PreProcessor, sliding_window_iter
@@ -10,12 +10,12 @@ class MacroData(Dataset):
     def __init__(self, 
             window_size: int, 
             data: pd.DataFrame,
-            dtype: torch.dtype=torch.float32,
+            # dtype: torchdtype=torch.float32,
             pad_val: float=-1e10) -> None:
         super().__init__()
         assert len(data) >= window_size, "data must be at least as logn as window_size!"
         self.data = data
-        self.data = self.data.fillna(pad_val)
+        self.pad_val = pad_val
         self.window_size = window_size
 
     def __iter__(self) -> Generator: 
@@ -24,8 +24,12 @@ class MacroData(Dataset):
     def __len__(self) -> int:
         return len(self.data) - self.window_size
     
-    def __getitem__(self, idx) -> np.array:
-        return self.data.iloc[idx:idx+self.window_size].values
+    def __getitem__(self, idx) -> Tuple[int, np.array, np.array]:
+        data = self.data.iloc[idx:idx+self.window_size]
+        
+        padding_mask = data.isna().sum(axis=1).apply(lambda x: True if x >=1 else False)
+        data = data.fillna(self.pad_val)
+        return idx, data.values, padding_mask.values
 
 
 
--- src/macroEncoding/deepLearning/models/transformerAE.py
+++ src/macroEncoding/deepLearning/models/transformerAE.py
@@ -39,8 +39,8 @@ class AutoEncoder(nn.Module):
         self.transformer_decoder = nn.TransformerDecoder(decoder_layer=decoder_layer, num_layers=num_transformer_layers)
         self.tanh = nn.Tanh()
 
-    def encode(self, x: torch.tensor) -> Tuple[torch.tensor]:
-        x_ = self.transformer_encoder(x)
+    def encode(self, x: torch.tensor, padding_mask: torch.tensor) -> Tuple[torch.tensor]:
+        x_ = self.transformer_encoder(x, src_key_padding_mask=padding_mask)
         # x_ = torch.flatten(x_, 1, 2)
         z = self.linear_encoder(x_)
         z = self.tanh(z)
@@ -51,10 +51,10 @@ class AutoEncoder(nn.Module):
         y = self.transformer_decoder(y_, memory=memory)
         return y
 
-    def forward(self, x) -> torch.tensor:
-        x_, z = self.encode(x)
+    def forward(self, x, padding_mask: torch.tensor) -> torch.tensor:
+        x_, z = self.encode(x, padding_mask=padding_mask)
         y_ = self.decode(z, x_)
         return y_
     
-    def __call__(self, x) -> torch.tensor:
-        return self.forward(x)
+    def __call__(self, x, padding_mask) -> torch.tensor:
+        return self.forward(x, padding_mask)
